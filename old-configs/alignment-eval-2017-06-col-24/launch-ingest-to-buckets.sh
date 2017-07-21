2d78104bac433aead54df5b062d707a8b49cc9d4
Stuart Berg <bergs@janelia.hhmi.org>
2017-07-21 14:28:09 -0400

    Moved old configs to a subdirectory

---
+++ old-configs/alignment-eval-2017-06-col-24/launch-ingest-to-buckets.sh
@@ -0,0 +1,22 @@
+if [[ $1 == "" ]]; then
+    1>&2 echo "Usage: $0 <num-workers> <test-index>"
+    exit 1
+fi
+
+if [[ $2 == "" ]]; then
+    1>&2 echo "Usage: $0 <num-workers> <test-index>"
+    exit 1
+fi
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd /groups/flyem/proj/cluster/miniconda/envs/flyem/DVIDServicesServer/SparkLaunch/janelia_sge
+
+source sparkenv.sh
+
+./sparklaunch_janelia $1 \
+    Ingest3DVolume \
+    ingest-to-buckets-$2 \
+    ${SCRIPT_DIR}/ingest-to-buckets.json \
+    /groups/flyem/proj/cluster/miniconda/envs/flyem/DVIDSparkServices/workflows/launchworkflow.py \
+    /groups/flyem/proj/cluster/miniconda/envs/flyem/bin/python \
+##
